e5e03a59317a517ebb6cf3d79b8b4370cb4eaf7a
Chadwick <chadwick@dreamlogic.in>
2022-04-20 11:22:03 +0530

    fixed api routes register user endpoint for app

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Crypt;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'name'              => 'required',
+            'password'          => 'required',
+            'phone_no'          => 'required|max:10|unique:users',
+            'vehicle_reg_no'    => 'required',
+            'car_model'         => 'required'
+        ]);
+        $data = [
+            'name'              => $request->name,
+            'password_hash'     => Crypt::encryptString($request->password),
+            'phone_no'          => $request->phone_no,
+            'vehicle_reg_no'    => $request->vehicle_reg_no,
+            'car_model'         => $request->car_model,
+        ];   
+        // creating user object to store data since password_hash is a hidden field, thus cannot be assigned directly using create($data)  
+        $user = new User($data);    
+        $user->password_hash = $data["password_hash"];
+        $user->save();
+        return response()->json(['message' => 'User Created Successfully'], 201);
+    }
+}
--- routes/api-routes.php
+++ routes/api-routes.php
@@ -13,24 +13,13 @@
 |
 */
 
-$router->group(['prefix' => 'admin'], function () use ($router) {
-    $router->get('login', function () {
-        return view('auth/login');
-    });  
-    $router->post('auth/login', 'Admin\AuthController@login');
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->post('auth/register','Api\AuthController@register');
 
-    $router->get('home', function () {
-        return view('pages/home', ['path'=>'home']);
-    });
+    $router->get('get-user-details/{vehicle_reg_no}', 'Api\UserController@getUserDetails');
 
-    $router->get('packages', 'Admin\PackageController@listPackages');
-    $router->post('create-package', 'Admin\PackageController@createPackage');
-    $router->put('update-package/{id}', 'Admin\PackageController@updatePackage');
-    $router->delete('delete-package/{id}', 'Admin\PackageController@deletePackage');
+    $router->get('get-package', 'Api\PackageController@getPackage');
 
-    $router->get('users', 'Admin\UserController@listUsers');  
-    $router->post('create-user', 'Admin\UserController@createUser');
-    $router->put('update-user/{id}', 'Admin\UserController@updateUser');
-    $router->delete('delete-user/{id}', 'Admin\UserController@deleteUser');
-   
+    $router->get('get-user-transactions/{user_id}', 'Api\TransactionController@getUserTransactions');
+    $router->post('buy-pass', 'Api\TransactionController@buyPass');   
 });
